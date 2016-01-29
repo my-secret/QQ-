68d969fde36c1e5da885e9459a6d5b7678991ff0
刘炜 <liuwei@liuweidemacbook-air.local>
2016-01-29 10:16:07 +0800

    Initial Commit

---
+++ QQ效果/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QQ效果
+//
+//  Created by 刘炜 on 16/1/29.
+//  Copyright © 2016年 刘炜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
